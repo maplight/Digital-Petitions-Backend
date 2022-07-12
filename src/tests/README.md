07f404816ec4183e789f34721a97c39693d3aa67
David R. Mollineda <drmollineda@gmail.com>
2022-07-11 20:37:04 -0400

    add dependencies for unit tests

---
+++ src/tests/README.md
@@ -0,0 +1,3 @@
+# Tests
+
+Place tests here. This includes data required for mocking services.
---
+++ src/tests/functions/sample.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from "@jest/globals";
+
+test("test data", () => {
+    expect(10).toBeTruthy();
+});
