5cff35918fd87a31b60e83f2252dea06bf03ffd8
David R. Mollineda <drmollineda@gmail.com>
2022-08-30 15:10:38 -0400

    DP-212 Update event types

--- auth/lambda/admin/types.ts
+++ auth/lambda/admin/types.ts
@@ -1,14 +1,24 @@
 export type HasEmail = { email: string };
 export type HasUserPoolId = { userPoolId: string };
+export type HasUserTarget = { username: string };
+
 export type EventCommon = HasEmail & HasUserPoolId;
+
+export type StaffAccessType = Exclude<AccessGroupKeys, "petitioner">;
+
+export type NewUserInput = {
+    firstName?: string;
+    lastName?: string;
+    permissions: StaffAccessType;
+} & HasEmail;
 
 export type NewUserEvent = EventCommon & {
     type: "new-user";
-};
+} & NewUserInput;
 
-export type DeleteUserEvent = EventCommon & {
+export type DeleteUserEvent = HasUserPoolId & {
     type: "delete-user";
-};
+} & HasUserTarget;
 
 export type AdminLambdaEvent = { payload: NewUserEvent | DeleteUserEvent };
 
@@ -16,5 +26,6 @@ import type {
     AdminCreateUserCommand,
     AdminDeleteUserCommand
 } from "@aws-sdk/client-cognito-identity-provider";
+import type { AccessGroupKeys } from "../common";
 
 export type CognitoCommand = AdminCreateUserCommand | AdminDeleteUserCommand | null;
