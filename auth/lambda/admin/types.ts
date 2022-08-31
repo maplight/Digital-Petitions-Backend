5bd86d5e2202f2e25f27929192d2b81db6d742cf
David R. Mollineda <drmollineda@gmail.com>
2022-08-30 20:32:30 -0400

    DP-212 Refactor and remove unused types

--- auth/lambda/admin/types.ts
+++ auth/lambda/admin/types.ts
@@ -1,16 +1,18 @@
+import type { AccessGroupKeys } from "../common";
+
 export type HasEmail = { email: string };
 export type HasUserPoolId = { userPoolId: string };
 export type HasUserTarget = { username: string };
 
 export type EventCommon = HasEmail & HasUserPoolId;
 
-export type StaffAccessType = Exclude<AccessGroupKeys, "petitioner">;
+export type StaffAccessType = Exclude<AccessGroupKeys, AccessGroupKeys.Petitioner>;
 
 export type NewUserInput = {
     firstName?: string;
     lastName?: string;
     permissions: StaffAccessType;
-} & HasEmail;
+};
 
 export type NewUserEvent = EventCommon & {
     type: "new-user";
@@ -21,11 +23,3 @@ export type DeleteUserEvent = HasUserPoolId & {
 } & HasUserTarget;
 
 export type AdminLambdaEvent = { payload: NewUserEvent | DeleteUserEvent };
-
-import type {
-    AdminCreateUserCommand,
-    AdminDeleteUserCommand
-} from "@aws-sdk/client-cognito-identity-provider";
-import type { AccessGroupKeys } from "../common";
-
-export type CognitoCommand = AdminCreateUserCommand | AdminDeleteUserCommand | null;
