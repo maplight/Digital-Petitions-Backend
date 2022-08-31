b5064482c42b42b53e2888131dc2552ab4e04882
David R. Mollineda <drmollineda@gmail.com>
2022-08-31 13:20:53 -0400

    DP-211 Implement user search/list

--- auth/lambda/admin/user-select.ts
+++ auth/lambda/admin/user-select.ts
@@ -1,7 +1,73 @@
-import type { CognitoIdentityProviderClient } from "@aws-sdk/client-cognito-identity-provider";
-import type { SelectUserEvent } from "./types";
+import {
+    CognitoIdentityProviderClient,
+    ListUsersCommand,
+    ListUsersCommandInput
+} from "@aws-sdk/client-cognito-identity-provider";
+import type { AccessGroupKeys } from "../common";
+import type { SelectUserEvent, User, Option, Connection } from "./types";
+import { attributeListToUser, sanitizeIdentifier } from "./util";
+
+function checkNames(user: User, name?: string): boolean {
+    return (!name || user.firstName?.startsWith(name) || user.lastName?.startsWith(name)) ?? false;
+}
+
+function checkTypes(user: User, type?: AccessGroupKeys): boolean {
+    return !type || user.permissions === type;
+}
+
+function applyEventFilter(user: User, event: SelectUserEvent): boolean {
+    return checkNames(user, event.searchName) && checkTypes(user, event.searchType);
+}
 
 export async function handleSelectUser(
     event: SelectUserEvent,
     client: CognitoIdentityProviderClient
-) {}
+): Promise<Option<Connection<User>>> {
+    event.searchName = sanitizeIdentifier(event.searchName);
+
+    const clientSide = !!(event.searchName || event.searchType);
+    const results: User[] = [];
+
+    let lastToken: string | undefined;
+    const pageLimit = event.limit ?? 10;
+
+    let filter: string | undefined;
+
+    if (event.searchEmail) {
+        filter = `email = \"${event.searchEmail}\"`;
+    }
+
+    try {
+        do {
+            const input: ListUsersCommandInput = {
+                UserPoolId: event.userPoolId,
+                Filter: filter,
+                PaginationToken: lastToken
+            };
+
+            if (!clientSide) {
+                input.Limit = pageLimit;
+            }
+
+            const command = new ListUsersCommand({
+                UserPoolId: event.userPoolId,
+                Filter: filter,
+                PaginationToken: lastToken
+            });
+
+            const items = await client.send(command);
+            lastToken = items.PaginationToken;
+
+            const users = items.Users?.map((user) => attributeListToUser(user.Attributes)) ?? [];
+            const match = clientSide
+                ? users.filter((user) => applyEventFilter(user, event))
+                : users;
+
+            results.push(...match);
+        } while (lastToken && results.length < pageLimit);
+
+        return { error: "None", data: { items: results.slice(0, pageLimit), token: lastToken } };
+    } catch (err) {
+        return { error: (err as Error)?.name };
+    }
+}
