07c6c2f9af9ac3012c237a0228b9e3febb3d224d
David R. Mollineda <drmollineda@gmail.com>
2022-08-31 13:20:23 -0400

    DP-211 Add utility function

--- auth/lambda/admin/util.ts
+++ auth/lambda/admin/util.ts
@@ -1,3 +1,7 @@
+import type { AttributeType } from "@aws-sdk/client-cognito-identity-provider";
+import { AccessGroupKeys } from "../common";
+import type { User } from "./types";
+
 export const allNonLetters = /[^a-zA-Z]/g;
 
 export function emailToUserName(email: string): string {
@@ -9,4 +13,28 @@ export function emailToUserName(email: string): string {
 
 export function sanitizeIdentifier(value: string | undefined): string | undefined {
     return value?.trim()?.replace(allNonLetters, "");
+}
+
+export const standardToAppNameMapping: { [K: string]: keyof User } = {
+    "given_name": "firstName",
+    "family_name": "lastName",
+    "email": "email",
+    "username": "username",
+    "custom:access_group": "permissions"
+};
+
+export function attributeListToUser(attributes: AttributeType[] | undefined): User {
+    return (
+        (attributes ?? [])
+            .map(({ Name, Value }) => [standardToAppNameMapping[Name ?? ""], Value])
+            .filter(([name, _]) => !!name) as [keyof User, string | undefined][]
+    ).reduce((dict, [name, value]) => {
+        if (name === "permissions") {
+            dict.permissions = (value as AccessGroupKeys) ?? AccessGroupKeys.Petitioner;
+        } else {
+            dict[name] = value ?? "";
+        }
+
+        return dict;
+    }, {} as User);
 }
