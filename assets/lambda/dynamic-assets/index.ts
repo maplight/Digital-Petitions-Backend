290018d328bf4fd9126ae98803a988a57f292c2f
David R. Mollineda <drmollineda@gmail.com>
2022-09-06 08:27:40 -0400

    DP-218 Add routing for the get items action

--- assets/lambda/dynamic-assets/index.ts
+++ assets/lambda/dynamic-assets/index.ts
@@ -1,18 +1,24 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import type { Handler } from "aws-lambda";
+import { handleGetItems } from "./get-items";
 import { handleGetUploadUrl } from "./get-upload-url";
 import { handleGetCurrentVersion } from "./get-version";
-import { Actions, AssetsEvent } from "./types";
+import { Actions, AssetsEvent, ResourceListing } from "./types";
 
 const client = new S3Client({});
 
-export const handler: Handler<AssetsEvent> = async (event): Promise<string | null> => {
+export const handler: Handler<AssetsEvent> = async (
+    event
+): Promise<string | ResourceListing | null> => {
     switch (event.type) {
         case Actions.GetCurrentVersion:
             return await handleGetCurrentVersion(event, client);
 
         case Actions.GetUploadUrl:
             return await handleGetUploadUrl(event, client);
+
+        case Actions.GetResourceList:
+            return await handleGetItems(event, client);
 
         default:
             return null;
