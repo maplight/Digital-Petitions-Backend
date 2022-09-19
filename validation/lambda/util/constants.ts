0fb408789d2dcdf751843c5ab753b7e0b0f55d30
David R. Mollineda <drmollineda@gmail.com>
2022-09-19 13:53:58 -0400

    DP-260 Add mock test case

--- validation/lambda/util/constants.ts
+++ validation/lambda/util/constants.ts
@@ -1,23 +1,46 @@
-import type {
-    MethodAvailibility,
-    VoterRecordMatch,
-    VerificationResult
-} from "../types";
+import type { VoterRecordMatch, VerificationResult } from "../types";
 import { VerificationMethod } from "../types";
 
-export const NO_AVAILABLE_METHODS: MethodAvailibility = {
-    methods: {
-        [VerificationMethod.Call]: false,
-        [VerificationMethod.Mail]: false,
-        [VerificationMethod.Text]: false,
-        [VerificationMethod.Email]: false,
-        [VerificationMethod.StateId]: false
-    }
+export const NO_AVAILABLE_METHODS: Record<VerificationMethod, boolean> = {
+    [VerificationMethod.Call]: false,
+    [VerificationMethod.Mail]: false,
+    [VerificationMethod.Text]: false,
+    [VerificationMethod.Email]: false,
+    [VerificationMethod.StateId]: false
 };
 
+/**
+ * Default result for when no match is found for
+ * a given identity.
+ */
 export const NO_MATCH: VoterRecordMatch = {
     token: null,
-    ...NO_AVAILABLE_METHODS
+    methods: NO_AVAILABLE_METHODS
+};
+
+export const JOHN_SMITH_TOKEN: string = "jsmith";
+
+export const JOHN_SMITH_INFO = {
+    email: "john.smith42@yopmail.com",
+    id: "jsmith64-abc",
+    date: "1970-01-01"
+};
+
+/**
+ * Dummy match for testing purposes.
+ */
+export const JOHN_SMITH_MATCH: VoterRecordMatch = {
+    token: JOHN_SMITH_TOKEN,
+    methods: { ...NO_AVAILABLE_METHODS, EMAIL: true, STATE_ID: true }
+};
+
+/**
+ * Dummy test data
+ */
+export const JOHN_SMITH = {
+    match: JOHN_SMITH_MATCH,
+    info: JOHN_SMITH_INFO,
+    token: JOHN_SMITH_TOKEN
 };
 
 const BASE_ERROR: Omit<VerificationResult, "error"> = {
@@ -26,6 +49,12 @@ const BASE_ERROR: Omit<VerificationResult, "error"> = {
     code: ""
 };
 
+/**
+ * Error result.
+ *
+ * Use when the requested verification method is not available for the
+ * provided identity.
+ */
 export const METHOD_NOT_AVAILABLE: VerificationResult = {
     ...BASE_ERROR,
     error: "The supplied verification method is not available for this voter"
